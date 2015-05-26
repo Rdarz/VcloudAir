fe73c15c017f1cddfca7e9ee87da62ef5a69bdda
Rahul Darji <rahul.darji@raweng.com>
2015-05-26 18:51:53 +0530

    Animation

--- vcloudair/site/public/js/script.js
+++ vcloudair/site/public/js/script.js
@@ -7,18 +7,70 @@ $(document).ready(function($) {
 $("header").slideDown(1000);
 
 $("#menu").slideDown(1000);	
+
 	function sample(){
         var current= $('.slide');
-    $(current[indx]).fadeOut(1000,function(){indx++;if(indx>=current.length)indx=0;$(current[indx]).fadeIn(5000,sample)});
+    	$(current[indx]).fadeOut(1000,function(){indx++;if(indx>=current.length)indx=0;$(current[indx]).fadeIn(5000,sample)});
     }
 
 
 
 
 
-    $(".left").animate({"left":"-10%"}, 3800);
+
+
+
+
+var incabout=1;
+var incworks=1;
+
+
+
+
+
+
+$(".about").waypoint(function(){
+
+	var intervalId=setInterval(function(){
+	var el=$(".animblock-about");
+	if(el.length > 0){
+	 		$(".animblock-about:nth-child("+incabout+")").removeClass('notViewed').addClass('viewed');
+		 incabout++;
+		}
+		else
+			clearInterval(intervalId);
+},200);
+
+
+},{offset:'50%'});
+
+$(".works").waypoint(function(){
+
+	var intervalId=setInterval(function(){
+
+	var el=$(".animblock");
+	if(el.length > 0){	 
+	 		$(".animblock:nth-child("+incworks+")").removeClass('notViewed').addClass('viewed');
+		 incworks++;
+		}
+		else
+			clearInterval(intervalId);
+},200);
+
+
+},{offset:'50%'});
+
+$(".slider").waypoint(function(){
+
+    		$(".left").animate({"left":"-10%"}, 3800);
 			$(".left-overlay").animate({"width":"76%"},3800);
 			$(".right").animate({"left":"30.7%"}, 3000);
+
+
+},{offset:'50%'});
+
+
+
 });
 
 // $('.caption:first-child').fadeIn(2500);
