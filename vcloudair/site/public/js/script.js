e9ea95c38b8010e7bcad65891d92a7693551a80c
Rahul Darji <rahul.darji@raweng.com>
2015-05-28 10:31:58 +0530

    modify

--- vcloudair/site/public/js/script.js
+++ vcloudair/site/public/js/script.js
@@ -24,14 +24,21 @@ $("#menu").slideDown(1000);
 var incabout=1;
 var incworks=1;
 
-function loop(){
+setInterval(function() {
+  	$('body section .caption:nth-child(1)').css("animation","top-one 15s ease infinite");
+	$('body section .caption:nth-child(2)').css("animation","top-one 15s ease infinite");
+	$('body section .caption:nth-child(3)').css("animation","top-one 15s ease infinite");
 
-	$('body section .caption:nth-child(1)').css("animation","top-one 5s ease 5s");
-	$('body section .caption:nth-child(2)').css("animation","top-one 5s ease 10s");
-	$('body section .caption:nth-child(3)').css("animation","top-one 5s ease 15s");
-loop();
-};
-loop();
+},2000);
+
+// function loop(){
+
+// 	$('body section .caption:nth-child(1)').css("animation","top-one 5s ease 5s");
+// 	$('body section .caption:nth-child(2)').css("animation","top-one 5s ease 10s");
+// 	$('body section .caption:nth-child(3)').css("animation","top-one 5s ease 15s");
+// loop();
+// };
+// loop();
 
 $(".about").waypoint(function(){
 
@@ -75,7 +82,36 @@ $(".slider").waypoint(function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.animation .col-md-6 .azure').css("-webkit-transform","rotate(45deg) translateX(-90%) translateY(-25%)");
+
+
+
+
+
+
+
+
 });
+
+
+
 
 // $('.caption:first-child').fadeIn(2500);
 // //$('.caption:first-child').slideUp(1500);
